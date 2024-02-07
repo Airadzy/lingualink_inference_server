97e04c4d0033d53617a0097e1962811859a4726e
Adrian <airadzy@gmail.com>
2024-02-07 18:58:34 +0100

    made a small change

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import Cambridge_API
 app = Flask(__name__)
 
 
-@app.route("/generate-quiz",methods=["POST"])
+@app.route("/generate-quiz", methods=["POST"])
 def generate_quiz_file():
-    data = request.get_json()
-    json_file_path = data.get("json_file_path", "content.json")
+    json_file_path = request.get_json()
     quiz_data = Cambridge_API.model(json_file_path)
     return jsonify(quiz_data)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
