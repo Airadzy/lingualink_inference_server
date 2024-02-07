e88f00674cf1962600217187947bc0f2c5f005d6
Adrian <airadzy@gmail.com>
2024-02-07 17:09:09 +0100

    updated requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify, request
 import Cambridge_API
+
 app = Flask(__name__)
+
+
 @app.route("/generate-quiz")
 def generate_quiz_file():
     data = request.get_json()
-    json_file_path = data.get("json_file_path","content.json")
+    json_file_path = data.get("json_file_path", "content.json")
     quiz_data = Cambridge_API.model(json_file_path)
     return jsonify(quiz_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
