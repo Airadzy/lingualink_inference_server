4a8ced2f2c8998b1abf6e3118154243b9426bfdf
Tal Ramon <tal.ramon400@gmail.com>
2024-02-08 11:36:19 +0200

    Add CORS and update requirements

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import Cambridge_API
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/generate-quiz", methods=["POST"])
+@cross_origin()
 def generate_quiz_file():
     json_file_path = request.get_json()
     quiz_data = Cambridge_API.model(json_file_path)
